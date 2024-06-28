65d61943bc7fe1f1bfb4c403ad6754e5d204b126
inthelongestday <02aso0621@gmail.com>
2024-06-28 15:43:25 +0900

    refactor: 갤러리 기능을 RecyclerView 활용하도록 수정

--- app/src/main/java/com/example/madcamp24_week1/GalleryAdapter.java
+++ app/src/main/java/com/example/madcamp24_week1/GalleryAdapter.java
@@ -1,55 +1,40 @@
 package com.example.madcamp24_week1;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class GalleryAdapter extends BaseAdapter {
-    private Context context;
-    private List<GalleryDTO> imageList;
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ImageViewHolder> {
+    private final Context context;
+    private final List<GalleryDTO> imageList;
 
     public GalleryAdapter(Context context, List<GalleryDTO> imageList) {
         this.context = context;
         this.imageList = imageList;
     }
 
+    @NonNull
     @Override
-    public int getCount() {
-        return imageList.size();
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.gallery_item, parent, false);
+        return new ImageViewHolder(view);
     }
 
     @Override
-    public Object getItem(int position) {
-        return imageList.get(position);
-    }
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        GalleryDTO currentItem = imageList.get(position);
+        holder.imageView.setImageResource(currentItem.getImageResId());
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            imageView = new ImageView(context);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(300, 300));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(1, 1, 1, 1);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-        imageView.setImageResource(imageList.get(position).getImageResId());
-
-        // Image click to show in dialog
-        imageView.setOnClickListener(new View.OnClickListener() {
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 View dialogView = View.inflate(context, R.layout.dialog, null);
@@ -57,7 +42,7 @@ public class GalleryAdapter extends BaseAdapter {
                 ImageView ivPic = dialogView.findViewById(R.id.ivPic);
                 TextView textView = dialogView.findViewById(R.id.textView);
                 textView.setText("Write your memo here..");
-                ivPic.setImageResource(imageList.get(position).getImageResId());
+                ivPic.setImageResource(currentItem.getImageResId());
                 dlg.setTitle("View Photo");
                 dlg.setIcon(R.drawable.ic_launcher_foreground);
                 dlg.setView(dialogView);
@@ -66,6 +51,34 @@ public class GalleryAdapter extends BaseAdapter {
             }
         });
 
-        return imageView;
+        holder.itemView.post(new Runnable() {
+            @Override
+            public void run() {
+                // 뷰의 높이를 너비와 동일하게 설정하여 정사각형으로 만듦
+                int width = holder.itemView.getWidth();
+                holder.imageView.getLayoutParams().height = width;
+                holder.imageView.requestLayout();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.ivPic);
+        }
     }
 }
--- app/src/main/java/com/example/madcamp24_week1/GalleryFragment.java
+++ app/src/main/java/com/example/madcamp24_week1/GalleryFragment.java
@@ -1,23 +1,31 @@
 package com.example.madcamp24_week1;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private GalleryAdapter galleryAdapter;
+    private List<GalleryDTO> imageList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
-        GridView gridView = view.findViewById(R.id.gridView);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
 
         // Mock 이미지 데이터를 제공
         List<GalleryDTO> imageList = new ArrayList<>();
@@ -31,8 +39,20 @@ public class GalleryFragment extends Fragment {
         imageList.add(new GalleryDTO(R.drawable.pic8));
         imageList.add(new GalleryDTO(R.drawable.pic9));
         imageList.add(new GalleryDTO(R.drawable.pic10));
+        imageList.add(new GalleryDTO(R.drawable.pic11));
+        imageList.add(new GalleryDTO(R.drawable.pic12));
+        imageList.add(new GalleryDTO(R.drawable.pic13));
+        imageList.add(new GalleryDTO(R.drawable.pic14));
+        imageList.add(new GalleryDTO(R.drawable.pic15));
+        imageList.add(new GalleryDTO(R.drawable.pic16));
+        imageList.add(new GalleryDTO(R.drawable.pic17));
+        imageList.add(new GalleryDTO(R.drawable.pic18));
+        imageList.add(new GalleryDTO(R.drawable.pic19));
+        imageList.add(new GalleryDTO(R.drawable.pic20));
+        imageList.add(new GalleryDTO(R.drawable.pic21));
 
-        gridView.setAdapter(new GalleryAdapter(getContext(), imageList));
+        galleryAdapter = new GalleryAdapter(getContext(), imageList);
+        recyclerView.setAdapter(galleryAdapter);
         return view;
     }
 }
