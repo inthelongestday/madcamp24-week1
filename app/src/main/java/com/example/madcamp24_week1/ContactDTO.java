459dcec1a9cad4eb7d0f29072ee7d5c59b23c9d3
Cheoroo <195822@jnu.ac.kr>
2024-06-28 16:58:34 +0900

    feat: 이름 수정을 위한 setname

--- app/src/main/java/com/example/madcamp24_week1/ContactDTO.java
+++ app/src/main/java/com/example/madcamp24_week1/ContactDTO.java
@@ -21,6 +21,10 @@ public class ContactDTO {
         this.phone = formatPhone(phone);
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String formatPhone(String phone) {
         if (phone.length() == 10) {
             return String.format("%s-%s-%s", phone.substring(0, 3), phone.substring(3, 6), phone.substring(6, 10));
