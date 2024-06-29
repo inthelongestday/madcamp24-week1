592783b3336cacf4cc556a63851db4716d8b9ea1
Cheoroo <195822@jnu.ac.kr>
2024-06-29 15:51:10 +0900

    feat: 연락처 data 수정
    data 계층에서 연락처 데이터를 CRUD 하도록 수정하고 mock데이터를 추가

--- app/src/main/java/com/example/madcamp24_week1/ContactData.java
+++ app/src/main/java/com/example/madcamp24_week1/ContactData.java
@@ -4,11 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactData {
+    private static List<ContactDTO> contacts = new ArrayList<>();
+    private static int nextId = 1;
+
+    static {
+        addMockContacts();
+    }
+
     public static List<ContactDTO> getContacts() {
-        List<ContactDTO> contactDTOS = new ArrayList<>();
-        contactDTOS.add(new ContactDTO("김철호", "0104156780"));
-        contactDTOS.add(new ContactDTO("김철호1", "01065143210"));
-        contactDTOS.add(new ContactDTO("김철호2", "0105555555"));
-        return contactDTOS;
+        return contacts;
+    }
+
+    public static ContactDTO getContactById(int id) {
+        for (ContactDTO contact : contacts) {
+            if (contact.getId() == id) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public static void addContact(ContactDTO contact) {
+        contact.setId(nextId++);
+        contacts.add(contact);
+    }
+
+    public static void updateContact(ContactDTO updatedContact) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getId() == updatedContact.getId()) {
+                contacts.set(i, updatedContact);
+                return;
+            }
+        }
+    }
+
+    public static void deleteContact(int id) {
+        contacts.removeIf(contact -> contact.getId() == id);
+    }
+
+    private static void addMockContacts() {
+        contacts.add(new ContactDTO(nextId++, "김철호", "0104156780"));
+        contacts.add(new ContactDTO(nextId++, "김철호1", "01065143210"));
+        contacts.add(new ContactDTO(nextId++, "김철호2", "0105555555"));
     }
 }
