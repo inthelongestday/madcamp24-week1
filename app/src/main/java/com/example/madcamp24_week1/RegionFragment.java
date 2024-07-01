0c88ac76c9f45a57085746225e209897673892b7
inthelongestday <02aso0621@gmail.com>
2024-07-01 15:49:16 +0900

    feat: region 선택 UI 변경 및 로직 수정

--- app/src/main/java/com/example/madcamp24_week1/RegionFragment.java
+++ app/src/main/java/com/example/madcamp24_week1/RegionFragment.java
@@ -1,17 +1,15 @@
 package com.example.madcamp24_week1;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import java.util.List;
 
@@ -26,29 +24,46 @@ public class RegionFragment extends Fragment {
         regionList = RegionData.getRegions();
         Log.d("RegionFragment", "Regions loaded: " + regionList.size());
 
-        LinearLayout layout = view.findViewById(R.id.regionLayout);
-        for (RegionDTO region : regionList) {
-            Log.d("RegionFragment", "Adding button for region: " + region.getName());
+        ImageView ivSeoul = view.findViewById(R.id.regionSeoul);
+        ImageView ivGyeonggi = view.findViewById(R.id.regionGyeonggi);
+        ImageView ivGangwon = view.findViewById(R.id.regionGangwon);
+        ImageView ivChungcheong = view.findViewById(R.id.regionChungcheong);
+        ImageView ivGyeongsang = view.findViewById(R.id.regionGyeongsang);
+        ImageView ivJeolla = view.findViewById(R.id.regionJeolla);
+        ImageView ivJeju = view.findViewById(R.id.regionJeju);
 
-            // Custom layout for region button
-            View regionView = inflater.inflate(R.layout.region_item, layout, false);
-            ImageView imageView = regionView.findViewById(R.id.regionImage);
-            TextView textView = regionView.findViewById(R.id.regionName);
-
-            imageView.setImageResource(region.getImageResId());
-            textView.setText(region.getName());
-
-            regionView.setOnClickListener(v -> {
-                Log.d("RegionFragment", "Region selected: " + region.getId());
-                if (getActivity() instanceof OnRegionSelectedListener) {
-                    ((OnRegionSelectedListener) getActivity()).onRegionSelected(region.getId());
-                }
-            });
-
-            layout.addView(regionView);
-        }
+        ivSeoul.setOnClickListener(v -> regionClickHandler(regionList.get(0), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
+        ivGyeonggi.setOnClickListener(v -> regionClickHandler(regionList.get(1), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
+        ivGangwon.setOnClickListener(v -> regionClickHandler(regionList.get(2), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
+        ivChungcheong.setOnClickListener(v -> regionClickHandler(regionList.get(3), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
+        ivGyeongsang.setOnClickListener(v -> regionClickHandler(regionList.get(4), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
+        ivJeolla.setOnClickListener(v -> regionClickHandler(regionList.get(5), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
+        ivJeju.setOnClickListener(v -> regionClickHandler(regionList.get(6), ivSeoul, ivGyeonggi, ivGangwon, ivChungcheong, ivGyeongsang, ivJeolla, ivJeju));
 
         return view;
+    }
+
+    private void regionClickHandler(
+        RegionDTO region,
+        ImageView ivSeoul,
+        ImageView ivGyeonggi,
+        ImageView ivGangwon,
+        ImageView ivChungcheong,
+        ImageView ivGyeongsang,
+        ImageView ivJeolla,
+        ImageView ivJeju
+    ) {
+        if (getActivity() instanceof OnRegionSelectedListener) {
+            Log.d("RegionFragment", "this region name: " + region.getName());
+            ivSeoul.setVisibility(View.GONE);
+            ivGyeonggi.setVisibility(View.GONE);
+            ivGangwon.setVisibility(View.GONE);
+            ivChungcheong.setVisibility(View.GONE);
+            ivGyeongsang.setVisibility(View.GONE);
+            ivJeolla.setVisibility(View.GONE);
+            ivJeju.setVisibility(View.GONE);
+            ((OnRegionSelectedListener) getActivity()).onRegionSelected(region.getId());
+        }
     }
 
     public interface OnRegionSelectedListener {
