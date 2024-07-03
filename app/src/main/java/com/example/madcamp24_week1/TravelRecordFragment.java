6258a68b3f3a3eb7d2349767d858a39786a6ff13
Cheoroo <195822@jnu.ac.kr>
2024-07-03 11:07:38 +0900

    fix: 병합 후 기능 누락 수정
    카메라 동작 순서를 변경했던 #a844d8d 커밋의 변경사항이 누락되어 수정하였습니다

--- app/src/main/java/com/example/madcamp24_week1/TravelRecordFragment.java
+++ app/src/main/java/com/example/madcamp24_week1/TravelRecordFragment.java
@@ -1,31 +1,50 @@
 package com.example.madcamp24_week1;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 public class TravelRecordFragment extends Fragment {
 
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
     private static final String ARG_REGION_ID = "region_id";
     private RecyclerView recyclerView;
     private TravelRecordAdapter travelRecordAdapter;
     private List<TravelRecordDTO> travelRecordList;
     private LocalDate currentDate;
     private TextView tvCurrentMonth;
+    private Uri photoURI;
+    private ActivityResultLauncher<Intent> takePhotoLauncher;
 
     public static TravelRecordFragment newInstance(int regionId) {
         TravelRecordFragment fragment = new TravelRecordFragment();
@@ -39,6 +58,34 @@ public class TravelRecordFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_travel_record, container, false);
+
+        takePhotoLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == FragmentActivity.RESULT_OK) {
+                        if (photoURI != null) {
+                            String imageUri = photoURI.toString();
+                            // Open TravelRecordEditFragment for creating new record
+                            TravelRecordEditFragment editFragment = TravelRecordEditFragment.newInstance(-1, 0, imageUri, "", "", getArguments().getInt(ARG_REGION_ID));
+                            editFragment.setOnTravelRecordEditListener((id, imageResId, memo, date, regionId, imageUri1, taggedContacts) -> {
+                                TravelRecordDTO newRecord = new TravelRecordDTO(TravelRecordData.getNextId(), imageResId, memo, date, regionId, imageUri1);
+                                TravelRecordData.addTravelRecord(newRecord);
+
+                                // 태깅된 연락처 정보 저장
+                                for (ContactDTO contact : taggedContacts) {
+                                    TravelRecordContactDTO recordContact = new TravelRecordContactDTO(newRecord.getId(), contact.getId());
+                                    TravelRecordContactData.addTravelRecordContact(recordContact);
+                                }
+
+                                travelRecordList.add(newRecord);
+                                travelRecordAdapter.notifyItemInserted(travelRecordList.size() - 1);
+                            });
+                            editFragment.show(getParentFragmentManager(), "travel_record_add");
+                        }
+                    }
+                }
+        );
+
         return view;
     }
 
@@ -69,24 +116,7 @@ public class TravelRecordFragment extends Fragment {
         updateTravelRecordList();
 
         FloatingActionButton fab = view.findViewById(R.id.fab);
-        fab.setOnClickListener(v -> {
-            // Open TravelRecordEditFragment for creating new record
-            TravelRecordEditFragment editFragment = TravelRecordEditFragment.newInstance(-1, 0, "", "", "", getArguments().getInt(ARG_REGION_ID));
-            editFragment.setOnTravelRecordEditListener((id, imageResId, memo, date, regionId, imageUri, taggedContacts) -> {
-                TravelRecordDTO newRecord = new TravelRecordDTO(TravelRecordData.getNextId(), imageResId, memo, date, regionId, imageUri);
-                TravelRecordData.addTravelRecord(newRecord);
-
-                // 태깅된 연락처 정보 저장
-                for (ContactDTO contact : taggedContacts) {
-                    TravelRecordContactDTO recordContact = new TravelRecordContactDTO(newRecord.getId(), contact.getId());
-                    TravelRecordContactData.addTravelRecordContact(recordContact);
-                }
-
-                travelRecordList.add(newRecord);
-                travelRecordAdapter.notifyItemInserted(travelRecordList.size() - 1);
-            });
-            editFragment.show(getParentFragmentManager(), "travel_record_add");
-        });
+        fab.setOnClickListener(v -> cameraIntent());
     }
 
     private void updateMonthDisplay() {
@@ -150,6 +180,44 @@ public class TravelRecordFragment extends Fragment {
                     }
                 }
                 break;
+            }
+        }
+    }
+
+    private void cameraIntent() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CAMERA_PERMISSION);
+        } else {
+            launchCamera();
+        }
+    }
+
+    private void launchCamera() {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePhotoIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            String timeStamp = new DateFormat().format("yyyyMMdd_HHmmss", new Date()).toString();
+            String imageFileName = "JPEG_" + timeStamp + "_";
+
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, imageFileName);
+            photoURI = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            takePhotoLauncher.launch(takePhotoIntent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            } else {
+                Toast.makeText(getContext(), "Camera permission is required to take photos", Toast.LENGTH_SHORT).show();
             }
         }
     }
