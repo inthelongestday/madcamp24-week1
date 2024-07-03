55194c588be9300cb1134dfe368c7c6d4cb58d4d
inthelongestday <02aso0621@gmail.com>
2024-07-03 13:25:47 +0900

    feat: 연락처 정렬 기능 + 삭제 시 확인 dialog 추가

--- app/src/main/java/com/example/madcamp24_week1/ContactData.java
+++ app/src/main/java/com/example/madcamp24_week1/ContactData.java
@@ -1,6 +1,7 @@
 package com.example.madcamp24_week1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ContactData {
@@ -14,15 +15,20 @@ public class ContactData {
 
     static {
         //mock
+        contacts.add(new ContactDTO(nextId++, "곽빈","01027864251"));
+        contacts.add(new ContactDTO(nextId++, "김이겸","01056891245"));
+        contacts.add(new ContactDTO(nextId++, "김재환","01012385121"));
         contacts.add(new ContactDTO(nextId++, "김철호","01012312321"));
-        contacts.add(new ContactDTO(nextId++, "김철호2","01011233333"));
-        contacts.add(new ContactDTO(nextId++, "김철호3","01213133321"));
-
+        contacts.add(new ContactDTO(nextId++, "김택연","01057822851"));
+        contacts.add(new ContactDTO(nextId++, "양의지","01013133321"));
+        contacts.add(new ContactDTO(nextId++, "정수빈","01074835628"));
+        contacts.add(new ContactDTO(nextId++, "허경민","01027834551"));
     }
 
     public static void addContact(ContactDTO contact) {
         contact.setId(nextId++);
         contacts.add(contact);
+        sortContacts();
         notifyDataChanged();
     }
 
@@ -30,6 +36,7 @@ public class ContactData {
         for (int i = 0; i < contacts.size(); i++) {
             if (contacts.get(i).getId() == updatedContact.getId()) {
                 contacts.set(i, updatedContact);
+                sortContacts();
                 notifyDataChanged();
                 return;
             }
@@ -38,8 +45,11 @@ public class ContactData {
 
     public static boolean deleteContact(int id) {
         boolean removed = contacts.removeIf(contact -> contact.getId() == id);
+        if (removed) {
+            sortContacts();
+            notifyDataChanged();
+        }
         System.out.println("Deleted: " + removed);
-        notifyDataChanged();
         return removed; // 삭제 성공 여부를 반환
     }
 
@@ -50,6 +60,10 @@ public class ContactData {
             }
         }
         return null;
+    }
+
+    private static void sortContacts() {
+        Collections.sort(contacts);
     }
 
     private static void notifyDataChanged() {
